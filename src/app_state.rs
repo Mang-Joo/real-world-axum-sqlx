4cf580199d02adf7140878fc5d2225b868e42f21
Mang-Joo <wjwan0915@gmail.com>
2024-04-08 23:29:20 +0900

    feat : add secret key of AppState

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,31 +1,16 @@
-use std::sync::Arc;
-use clap::Parser;
-use sqlx::{MySql, Pool};
 use crate::db::DbPool;
 
 #[derive(Debug)]
 pub struct AppState {
     pub pool: DbPool,
-}
-
-#[derive(Parser, Debug)]
-pub struct Config {
-    #[arg(long)]
-    pub database_url: String,
-    #[arg(long)]
-    pub hmac_key: String,
-}
-
-impl Config {
-    pub fn database_url(&self) -> String {
-        String::from(&self.database_url)
-    }
+    pub secret_key: String,
 }
 
 impl AppState {
-    pub fn new(db_pool: DbPool) -> Self {
+    pub fn new(db_pool: DbPool, secret_key: String) -> Self {
         AppState {
-            pool: db_pool
+            pool: db_pool,
+            secret_key
         }
     }
 }
