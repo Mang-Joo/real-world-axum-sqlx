a54222b246fff52f5d91614a7be9a33406b73068
Mang-Joo <wjwan0915@gmail.com>
2024-04-15 17:33:41 +0900

    feat : Add AppError in handler.rs

--- src/user/handler.rs
+++ src/user/handler.rs
@@ -1,25 +1,21 @@
 use std::sync::Arc;
 
 use axum::extract::State;
-use axum::http::StatusCode;
 use axum::Json;
 use axum::response::IntoResponse;
-use axum_macros::debug_handler;
 
 use crate::app_state::AppState;
+use crate::error::AppError;
 use crate::user::application::login_usecase;
 use crate::user::application::login_usecase::LoginRequest;
 
-#[debug_handler]
 pub async fn login_user(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<LoginRequest>,
-) -> Result<impl IntoResponse, StatusCode> {
-
-    let response = login_usecase::login(state, payload)
+) -> Result<impl IntoResponse, AppError> {
+    let response = login_usecase::user_login(state, payload)
         .await
-        .map_err(|err| return StatusCode::BAD_REQUEST)
-        .unwrap();
+        .map_err(|err| return AppError::AnyHow(err))?;
 
     Ok(Json(response))
 }
