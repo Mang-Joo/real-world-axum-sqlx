4c6fb5ffd43ffa7e3f97a0ae7bde1b2c20e3509f
Mang-Joo <wjwan0915@gmail.com>
2024-04-15 17:34:44 +0900

    feat : Add login route in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::sync::Arc;
 
-use axum::{Extension, Router};
-use axum::response::IntoResponse;
-use axum::routing::get;
 use tokio::net::TcpListener;
 
-use crate::app_state::{AppState, init_app_state};
+use crate::app_state::init_app_state;
+use crate::user::route::login;
 
 mod app_state;
 mod db;
@@ -14,35 +12,14 @@ mod user;
 mod error;
 
 
-async fn handler(Extension(state): Extension<Arc<AppState>>) -> impl IntoResponse {
-    let query = r#"
-        CREATE TABLE users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    "#;
-    sqlx::query(query)
-        .execute(&state.pool)
-        .await
-        .ok();
-
-    println!("hello {:?}", state);
-    eprintln!("hello {:?}", state);
-    "helloWorld".into_response()
-}
-
-
 #[tokio::main]
 async fn main() {
     let app_state = init_app_state().await;
     let app_state = Arc::new(app_state);
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    let route = Router::new()
-        .route("/", get(handler))
-        .layer(Extension(app_state));
+
+    let route = login(app_state).await;
 
     axum::serve(listener, route).await.unwrap();
 }
