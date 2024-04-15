5374da75a2fc29210a84d123832b93d0a3a464b3
Mang-Joo <wjwan0915@gmail.com>
2024-04-15 17:36:19 +0900

    feat : Add error handler

--- src/user/route.rs
+++ src/user/route.rs
@@ -1,23 +1,15 @@
 use std::sync::Arc;
 
-use axum::{middleware, Router};
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
+use axum::{Extension, Router};
 use axum::routing::post;
 
 use crate::app_state::AppState;
+use crate::error::error_handler;
 use crate::user::handler::login_user;
 
 pub async fn login(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/login", post(login_user))
+        .layer(Extension(error_handler))
         .with_state(app_state)
-        // .layer(middleware::from_extractor())
-}
-
-async fn handle_error(err: anyhow::Error) -> impl IntoResponse {
-    (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        format!("Something went wrong: {}", err),
-    )
 }
