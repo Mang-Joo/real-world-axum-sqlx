057ffbc659d81eefb76e1e6a80c652ee2095330e
Mang-Joo <wjwan0915@gmail.com>
2024-04-15 17:33:59 +0900

    feat : Update user struct RefCell -> Arc

--- src/user/domain/hash_password.rs
+++ src/user/domain/hash_password.rs
@@ -1,4 +1,7 @@
-use anyhow::{anyhow, Context};
+use std::ops::Deref;
+use std::sync::Arc;
+
+use anyhow::anyhow;
 use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
 use argon2::password_hash::rand_core::OsRng;
 use argon2::password_hash::SaltString;
@@ -7,7 +10,7 @@ use axum::async_trait;
 #[async_trait]
 pub trait HashPassword {
     async fn hash(&self, plain_data: &String) -> anyhow::Result<String>;
-    async fn verify(&self, plain_data: String, hashed_data: &String) -> bool;
+    async fn verify(&self, plain_data: String, hashed_data: &Arc<String>) -> bool;
 }
 
 unsafe impl Send for ArgonHash {}
@@ -35,11 +38,21 @@ impl HashPassword for ArgonHash {
         Ok(hashed_data)
     }
 
-    async fn verify(&self, plain_data: String, hashed_data: &String) -> bool {
-        let argon2 = Argon2::default();
 
-        let hash = PasswordHash::new(&hashed_data)
-            .unwrap();
+    async fn verify(&self, plain_data: String, hashed_data: &Arc<String>) -> bool {
+        let argon2 = Argon2::default();
+        let hashed_data = Arc::clone(hashed_data);
+        let hashed_data = hashed_data.deref().to_string();
+
+        let hash = PasswordHash::new(&hashed_data);
+
+        let hash = match hash {
+            Ok(hash) => { hash }
+            Err(err) => {
+                eprintln!("Error {:?}", err);
+                return false;
+            }
+        };
 
         argon2.verify_password(plain_data.as_bytes(), &hash).is_ok()
     }
@@ -47,6 +60,8 @@ impl HashPassword for ArgonHash {
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Arc;
+
     use crate::user::domain::hash_password::{ArgonHash, HashPassword};
 
     #[tokio::test]
@@ -62,7 +77,8 @@ mod tests {
     #[tokio::test]
     async fn verify_test() {
         let hello = String::from("hello");
-        let hashed_data = String::from("$argon2id$v=19$m=19456,t=2,p=1$ULFfcwnYvCZwgiRm1i97yg$OcMjE44RqEVd4fzKFUJtuBJMsVEvQX2641nYX9ZCQDY");
+
+        let hashed_data = Arc::new(String::from("$argon2id$v=19$m=19456,t=2,p=1$ULFfcwnYvCZwgiRm1i97yg$OcMjE44RqEVd4fzKFUJtuBJMsVEvQX2641nYX9ZCQDY"));
 
         let response = ArgonHash::default().verify(hello, &hashed_data).await;
 
@@ -72,9 +88,10 @@ mod tests {
     #[tokio::test]
     async fn wrong_hashed_data_verify_test() {
         let hello = String::from("hello");
-        let hashed_data = String::from("=19$m=19456,t=2,p=1$ULFfcwnYvCZwgiRm1i97yg$OcMjE44RqEVd4fzKFUJtuBJMsVEvQX2641nYX9ZCQDY");
+        let hashed_data = Arc::new(String::from("$argon2id$v=19$m=19456,t=2,p=1$ULFfcwnYvCZwgiRm1i97yg$OcMjE44RqEVd4fzKFUJtuBJMsVEvQX2641nYX9ZCQDA"));
 
-        let response = ArgonHash::default().verify(hello, &hashed_data).await;
+        let response = ArgonHash::default()
+            .verify(hello, &hashed_data).await;
 
         assert_eq!(response, false);
     }
