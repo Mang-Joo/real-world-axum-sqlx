2612cfb0061280244db986ea7c078151e5e378dc
Mang-Joo <wjwan0915@gmail.com>
2024-04-22 17:54:36 +0900

    feat : Add log4rs setting

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ async fn main() {
     let app_state = init_app_state().await;
     let app_state = Arc::new(app_state);
 
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 
     let route = user(app_state).await;
