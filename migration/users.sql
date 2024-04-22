412b7e111465a3be9190fcdb1bba9d3b64548a7a
Mang-Joo <wjwan0915@gmail.com>
2024-04-22 17:56:30 +0900

    feat : Update user migration sql

--- migration/users.sql
+++ migration/users.sql
@@ -1,15 +1,25 @@
+DROP TABLE users;
+
+
 create table users
 (
-    id        BIGINT auto_increment,
-    email     VARCHAR(50)  not null,
-    user_name varchar(100) not null,
-    bio       varchar(500) null,
-    image     varchar(50)  null,
-    password  varchar(500) not null,
+    id                BIGINT auto_increment,
+    user_name         varchar(100) not null,
+    email             VARCHAR(50)  not null,
+    password          varchar(500) not null,
+    bio               varchar(500) null,
+    image             varchar(50)  null,
+    registration_date DATE         not null,
+    modified_date     DATE         not null,
+    deleted           bool         not null,
     constraint users_pk
         primary key (id)
 );
 
 create index users_email_index
     on users (email);
+
+create index users_username_index
+    on users (user_name);
+
 
