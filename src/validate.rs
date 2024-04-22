63c03c967e169e8e817b2f21d64309ef72e2a8a0
Mang-Joo <wjwan0915@gmail.com>
2024-04-22 17:56:44 +0900

    feat : Update extractor for jwt

--- src/validate.rs
+++ src/validate.rs
@@ -2,7 +2,8 @@ use std::sync::Arc;
 
 use anyhow::anyhow;
 use axum::{async_trait, Json};
-use axum::extract::{FromRequest, Request};
+use axum::extract::{FromRequest, FromRequestParts, Request};
+use axum::http::request::Parts;
 use serde::de::DeserializeOwned;
 use validator::Validate;
 
@@ -10,8 +11,9 @@ use crate::app_state::AppState;
 use crate::auth::jwt_decoder::JwtDecoder;
 use crate::error::AppError;
 
+const AUTHORIZATION: &str = "Authorization";
+
 #[derive(Debug, Clone, Copy, Default)]
-#[must_use]
 pub struct ValidationExtractor<T>(pub T);
 
 
@@ -38,16 +40,18 @@ impl<T, S> FromRequest<S> for ValidationExtractor<T>
     }
 }
 
+#[derive(Debug, Clone, Copy, Default)]
 pub struct JwtValidationExtractor(pub i64);
 
 #[async_trait]
-impl<S> FromRequest<S> for JwtValidationExtractor
+impl<S> FromRequestParts<S> for JwtValidationExtractor
     where
-        S: Send + Sync, {
+        S: Send + Sync,
+{
     type Rejection = AppError;
 
-    async fn from_request(req: Request, _state: &S) -> Result<Self, Self::Rejection> {
-        let token = if let Some(header) = req.headers().get("Authorization") {
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let token = if let Some(header) = parts.headers.get(AUTHORIZATION) {
             header.to_str()
                 .map_err(|_| anyhow!("Invalid header value"))?
                 .replace("Bearer ", "")
@@ -55,14 +59,47 @@ impl<S> FromRequest<S> for JwtValidationExtractor
             return Err(AppError::Unauthorized);
         };
 
-        let app_state = req.extensions().get::<Arc<AppState>>().ok_or_else(|| {
-            return AppError::AnyHow(anyhow!("Can't get state"));
-        })
-            .map_err(|err| { return err; })?;
+
+        let app_state = get_app_state(parts);
 
         let jwt_decoder = JwtDecoder::new(app_state.to_owned());
         let payload = jwt_decoder.decode_token(&token).await?;
 
         Ok(JwtValidationExtractor(payload.id()))
     }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct OptionalAuthenticateExtractor(pub Option<i64>);
+
+impl<S> FromRequestParts<S> for OptionalAuthenticateExtractor
+    where
+        S: Send + Sync {
+    type Rejection = AppError;
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let token = if let Some(header) = parts.headers.get(AUTHORIZATION) {
+            header.to_str()
+                .replace("Bearer ", "")
+        } else {
+            return Ok(None);
+        };
+
+
+        let app_state = Self::get_app_state(parts)?;
+
+        let jwt_decoder = JwtDecoder::new(app_state.to_owned());
+        let payload = jwt_decoder.decode_token(&token).await?;
+
+        Ok(JwtValidationExtractor(payload.id()))
+    }
+}
+
+fn get_app_state(parts: &mut Parts) -> Result<&Arc<AppState>, AppError> {
+    let app_state = parts.extensions.get::<Arc<AppState>>().ok_or_else(|| {
+        return AppError::AnyHow(anyhow!("Can't get _state"));
+    })
+        .map_err(|err| { return err; })?;
+
+    Ok(app_state)
 }
