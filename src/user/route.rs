5bf56de394ac0635d84645daeae0a2c220ca5ceb
Mang-Joo <wjwan0915@gmail.com>
2024-04-23 17:49:17 +0900

    :sparkles: feat: Separate route

--- src/user/route.rs
+++ src/user/route.rs
@@ -5,7 +5,8 @@ use axum::routing::{get, post, put};
 
 use crate::app_state::AppState;
 use crate::error::error_handler;
-use crate::user::application::handler::{get_current_user, login_user, registration_user, update_user_handler};
+use crate::user::application::follow_handler::{follow_user_handler, get_profile_handler};
+use crate::user::application::user_handler::{get_current_user, login_user, registration_user, update_user_handler};
 
 pub async fn user(app_state: Arc<AppState>) -> Router {
     Router::new()
@@ -13,6 +14,18 @@ pub async fn user(app_state: Arc<AppState>) -> Router {
         .route("/users", post(registration_user))
         .route("/users", get(get_current_user))
         .route("/users", put(update_user_handler))
+}
+
+pub async fn follow(app_state: Arc<AppState>) -> Router {
+    Router::new()
+        .route("/profiles/:username", get(get_profile_handler))
+        .route("/profiles/:username/follow", post(follow_user_handler))
+}
+
+pub async fn route(app_state: Arc<AppState>) -> Router {
+    Router::new()
+        .nest("/api", follow(app_state.clone()).await)
+        .nest("/api", user(app_state.clone()).await)
         .layer(Extension(error_handler))
         .layer(Extension(app_state))
 }
