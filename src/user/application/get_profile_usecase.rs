f3a889f29a209031ec3190cfe6383b4e1867a199
Mang-Joo <wjwan0915@gmail.com>
2024-04-23 17:47:34 +0900

    :sparkles: feat : get profile implementation

--- src/user/application/get_profile_usecase.rs
+++ src/user/application/get_profile_usecase.rs
@@ -2,17 +2,21 @@ use std::sync::Arc;
 
 use crate::app_state;
 use crate::app_state::AppState;
-use crate::user::application::repository::find_by_user_name;
+use crate::user::application::follow_repository::is_follow;
+use crate::user::application::user_repository::find_by_user_name;
 use crate::user::domain::user::User;
 
 pub async fn get_profile(
     app_state: Arc<AppState>,
     user_id: Option<i64>,
     user_name: String,
-) -> app_state::Result<User> {
-    let user = find_by_user_name(&user_name, &app_state.pool).await?;
+) -> app_state::Result<(User, bool)> {
+    let target_user = find_by_user_name(&user_name, &app_state.pool).await?;
 
-    
+    let is_follow: bool = if let Some(user_id) = user_id {
+        is_follow(user_id, target_user.id(), &app_state.pool).await
+    } else { false };
 
-    todo!()
+    Ok((target_user, is_follow))
 }
+
