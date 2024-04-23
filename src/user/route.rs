c343064795915874b161991972eedacea4c3a8eb
Mang-Joo <wjwan0915@gmail.com>
2024-04-23 17:53:44 +0900

    :sparkles: feat : Remove not use app_state

--- src/user/route.rs
+++ src/user/route.rs
@@ -8,7 +8,7 @@ use crate::error::error_handler;
 use crate::user::application::follow_handler::{follow_user_handler, get_profile_handler};
 use crate::user::application::user_handler::{get_current_user, login_user, registration_user, update_user_handler};
 
-pub async fn user(app_state: Arc<AppState>) -> Router {
+pub async fn user() -> Router {
     Router::new()
         .route("/login", post(login_user))
         .route("/users", post(registration_user))
@@ -16,7 +16,7 @@ pub async fn user(app_state: Arc<AppState>) -> Router {
         .route("/users", put(update_user_handler))
 }
 
-pub async fn follow(app_state: Arc<AppState>) -> Router {
+pub async fn follow() -> Router {
     Router::new()
         .route("/profiles/:username", get(get_profile_handler))
         .route("/profiles/:username/follow", post(follow_user_handler))
@@ -24,8 +24,8 @@ pub async fn follow(app_state: Arc<AppState>) -> Router {
 
 pub async fn route(app_state: Arc<AppState>) -> Router {
     Router::new()
-        .nest("/api", follow(app_state.clone()).await)
-        .nest("/api", user(app_state.clone()).await)
+        .nest("/api", follow().await)
+        .nest("/api", user().await)
         .layer(Extension(error_handler))
         .layer(Extension(app_state))
 }
