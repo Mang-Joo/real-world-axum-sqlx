2e28cf462286d23cfb169bee8a9cfd36bfd5d4b7
Mang-Joo <wjwan0915@gmail.com>
2024-04-30 00:41:45 +0900

    :white_check_mark: test : get article integration test

--- tests/article_integration.rs
+++ tests/article_integration.rs
@@ -1,14 +1,15 @@
-use axum::body::Body;
+use axum::body::{Body, to_bytes};
 use axum::http::{Method, Request, StatusCode};
-use serde_json::json;
+use axum::response::Response;
+use serde_json::{json, Value};
 use tower::ServiceExt;
 
-use crate::common::fixture_route;
+use crate::common::{fixture_route, ResponseData};
 
 mod common;
 
 #[tokio::test]
-pub async fn save_article_200_Ok() {
+pub async fn save_article_200_ok() {
     let request_body = json!({
         "title": "Hello mangjoo ",
         "description": "Test Description",
@@ -57,4 +58,38 @@ pub async fn save_article_400_bad_request() {
         .unwrap();
 
     assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+}
+
+#[tokio::test]
+async fn get_single_article_200_ok() {
+    let slug = "Hello-mangjoo-";
+    let request = Request::builder()
+        .uri(format!("/api/articles/{}", slug))
+        .method(Method::GET)
+        .header("Content-Type", "application/json")
+        // .header("Authorization", common::TOKEN_FIXTURE)
+        .body(Body::empty())
+        .unwrap();
+
+    let response: Response = fixture_route()
+        .await
+        .oneshot(request)
+        .await
+        .unwrap();
+
+    let byte = to_bytes(response.into_body(), usize::MAX).await
+        .expect("");
+
+    let result: ResponseData = serde_json::from_slice(&byte).unwrap();
+
+
+    let data = result.data
+        .get("slug").unwrap();
+
+    let data = match data {
+        None => { Value::Null }
+        Some(data) => { data.to_owned() }
+    };
+
+    assert_eq!(data, slug);
 }
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use axum::Router;
+use serde::Deserialize;
+use serde_json::Value;
 
 use real_world_axum_sqlx::config::app_state::init_app_state;
 use real_world_axum_sqlx::create_route;
@@ -10,4 +13,10 @@ pub const TOKEN_FIXTURE: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NS
 pub async fn fixture_route() -> Router {
     let app_state = Arc::new(init_app_state().await);
     create_route(app_state).await
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ResponseData {
+    #[serde(flatten)]
+    pub data: HashMap<String, Option<Value>>,
 }
