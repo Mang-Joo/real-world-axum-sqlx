f2a18f66e20ba24b19c86de7185b1da75682bb3b
Mang-Joo <wjwan0915@gmail.com>
2024-04-30 00:41:35 +0900

    :sparkles: feat : Add get single article route

--- src/article/route.rs
+++ src/article/route.rs
@@ -1,8 +1,9 @@
 use axum::Router;
-use axum::routing::post;
-use crate::article::handler::create_article_handler;
+use axum::routing::{get, post};
+use crate::article::handler::{create_article_handler, get_single_article_handler};
 
 pub async fn article_route() -> Router {
     Router::new()
         .route("/articles", post(create_article_handler))
+        .route("/articles/:slug", get(get_single_article_handler))
 }
