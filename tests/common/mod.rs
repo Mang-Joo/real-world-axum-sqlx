a5af44e67b9b7c1703c5d3d840871d00d8f67509
Mang-Joo <wjwan0915@gmail.com>
2024-04-29 18:00:04 +0900

    :white_check_mark: test : Update fixture route

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,12 +1,12 @@
 use std::sync::Arc;
-
 use axum::Router;
-
+use real_world_axum_sqlx::config::app_state;
 use real_world_axum_sqlx::config::app_state::init_app_state;
-use real_world_axum_sqlx::user::route::user_route;
+use real_world_axum_sqlx::create_route;
 
-pub const TOKEN_FIXTURE: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwic3ViIjoid2p3YW4wOTE2QGdtYWlsLmNvbSIsImV4cCI6MTc0NTQ1NzU2NCwiaWF0IjoxNzEzOTIxNTY0fQ.VdGRZ9g-GLbrDJRJVmHoY-Cm2ZYFgstj2AeAXmvh4pI";
+pub const TOKEN_FIXTURE: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwic3ViIjoid2p3YW4wOTE2QGdtYWlsLmNvbSIsImV4cCI6MTcxNDYxMDk2MiwiaWF0IjoxNzE0MzUxNzYyfQ._tIcuy5anTm3VifuB4flj9i8lUu4cL0Qxs_CbxN0Wrs";
 
 pub async fn fixture_route() -> Router {
-    user_route().await
+    let app_state = Arc::new(init_app_state().await);
+    create_route(app_state).await
 }
