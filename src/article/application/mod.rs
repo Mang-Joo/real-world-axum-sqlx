3859aba74b600d3f70be9729aaec57f128cb3d19
Mang-Joo <wjwan0915@gmail.com>
2024-04-30 00:41:02 +0900

    :sparkles: feat : modify usecase access control

--- src/article/application/mod.rs
+++ src/article/application/mod.rs
@@ -1,5 +1,6 @@
 pub mod create_article_usecase;
+pub mod get_single_article_usecase;
 mod article_repository;
 mod tag_repository;
 mod article_tag_repository;
-mod get_single_article_usecase;
+mod article_favorite_repository;
