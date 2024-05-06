ec49b6be320c3dba1f48cbe2ab565e9356dbf70d
Mang-Joo <wjwan0915@gmail.com>
2024-05-06 18:30:43 +0900

    :sparkles: feat : changed db

--- src/article/application/article_favorite_repository.rs
+++ src/article/application/article_favorite_repository.rs
@@ -1,50 +1,31 @@
-use anyhow::{anyhow, Context};
-use log::{error, info};
+use std::sync::Arc;
+
+use anyhow::Context;
 
 use crate::config;
-use crate::config::db::DbPool;
+use crate::config::app_state::AppState;
 
-pub async fn count_favorite_by_article_id(
-    article_id: i64,
-    db_pool: &DbPool,
-) -> config::Result<i64> {
-    let count: i64 = sqlx::query_scalar("
-        SELECT count(*)
-        FROM article_favorite
-        WHERE article_id = ?"
-    )
-        .bind(article_id)
-        .fetch_one(db_pool)
-        .await
-        .map_err(|err| {
-            error!("Failed get count by article {}", err.to_string());
-            anyhow!("Failed get count by article")
-        })?;
-
-    info!("Succeed get count by article id : {article_id}");
-
-    Ok(count)
-}
-
-pub async fn is_favorite(
+pub async fn is_favorite_article(
     user_id: i64,
     article_id: i64,
-    db_pool: &DbPool,
+    app_state: Arc<AppState>,
 ) -> config::Result<bool> {
-    let result = sqlx::query("
+    let result = sqlx::query(
+        "
     SELECT 1
     FROM article_favorite
-    WHERE article_id = ?
-    AND user_id = ?
-    ")
-        .bind(article_id)
-        .bind(user_id)
-        .fetch_optional(db_pool)
-        .await
-        .context("Failed select is favorite")?;
+    WHERE article_id = $1
+    AND favorite_user_id = $2
+    ",
+    )
+    .bind(article_id)
+    .bind(user_id)
+    .fetch_optional(&app_state.pool)
+    .await
+    .context("Failed select is favorite")?;
 
     match result {
-        None => { Ok(false) }
-        Some(_) => { Ok(true) }
+        None => Ok(false),
+        Some(_) => Ok(true),
     }
 }
