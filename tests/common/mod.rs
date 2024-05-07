a6fc02116a7ebc746daaac82b11ea111ea82321b
Mang-Joo <wjwan0915@gmail.com>
2024-05-07 18:03:39 +0900

    :white_check_mark: test : token update 1 year

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -8,7 +8,7 @@ use serde_json::Value;
 use real_world_axum_sqlx::config::app_state::init_app_state;
 use real_world_axum_sqlx::create_route;
 
-pub const TOKEN_FIXTURE: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwic3ViIjoid2p3YW4wOTE2QGdtYWlsLmNvbSIsImV4cCI6MTcxNDYxMDk2MiwiaWF0IjoxNzE0MzUxNzYyfQ._tIcuy5anTm3VifuB4flj9i8lUu4cL0Qxs_CbxN0Wrs";
+pub const TOKEN_FIXTURE: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwic3ViIjoid2p3YW4wOTE1QGdtYWlsLmNvbSIsImV4cCI6MTc0NjU5ODYwOCwiaWF0IjoxNzE1MDYyNjA4fQ.AvdXtrVkm7jYk0xi4RTdrT9n0d3j1SgJ-V-i5KsNQ80";
 
 pub async fn fixture_route() -> Router {
     let app_state = Arc::new(init_app_state().await);
