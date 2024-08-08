3d8e77951d9112a35f7cb2aa7440e645f4273f71
Mang-Joo <wjwan0915@gmail.com>
2024-08-08 14:34:09 +0900

    🔧 chore : Update user sql

--- migration/users.sql
+++ migration/users.sql
@@ -3,18 +3,18 @@ DROP TABLE users;
 create table users
 (
     id                BIGSERIAL PRIMARY KEY,
-    user_name         varchar(100) not null,
-    email             VARCHAR(50)  not null,
-    password          varchar(500) not null,
-    bio               varchar(500) null,
-    image             varchar(50) null,
-    registration_date timestamp    not null,
-    modified_date     timestamp    not null,
-    deleted           bool         not null
+    username          varchar(100)            not null,
+    email             VARCHAR(50)             not null,
+    password          varchar(500)            not null,
+    bio               varchar(500)            null,
+    image             varchar(50)             null,
+    registration_date timestamp default now() not null,
+    modified_date     timestamp default now() not null,
+    deleted           bool      default false not null
 );
 
 create index users_email_index
     on users (email);
 
 create index users_username_index
-    on users (user_name);
+    on users (username);
